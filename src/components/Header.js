fbe5426798b4b614475e02574d46b7a5544b363a
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-03-24 13:58:31 +0900

    feat:로그인 상태

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,27 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 import { removeCookie } from "../function/cookies";
+import { useSelector, useDispatch } from "react-redux";
+import { DELETE_TOKEN } from "../store/Auth";
 const Header = () => {
-  const [isLogin, setIsLogin] = useState(false);
   const navigation = useNavigate();
-  const LogoutHandler = () => {
-    removeCookie("");
-    sessionStorage.removeItem("access");
-    sessionStorage.removeItem("accessTime");
-    setIsLogin(false);
-    navigation("/");
+  // const LogoutHandler = () => {
+  //   removeCookie("");
+  //   sessionStorage.removeItem("access");
+  //   sessionStorage.removeItem("accessTime");
+  //   setIsLogin(false);
+  //   navigation("/");
+  // };
+
+  //로그인&로그아웃
+
+  const authenticated = useSelector((state) => state.authToken.authenticated);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    // 로그아웃 요청 처리
+    dispatch(DELETE_TOKEN());
   };
-
-  useEffect(() => {
-    // if(){
-    //   setIsLogin(true);
-    // }
-  }, []);
-
   return (
     <div className="relative bg-main font-extrabold">
       <div className="border-b-zinc-400 border-t-stone-600 flex h-20  items-center justify-between border-t-4 border-b-2 ">
@@ -28,10 +32,10 @@ const Header = () => {
         <div className="flex items-center gap-40">
           <FiSearch className="font-bold text-red-500" />
           <Link to={"/Board"}>Board</Link>
-          {isLogin ? (
+          {authenticated ? (
             <>
               <Link to={"/MyPage:userId"}>MyPage</Link>
-              <button onClick={LogoutHandler}>Logout</button>
+              <button onClick={handleLogout}>Logout</button>
             </>
           ) : (
             <Link to={"/LoginForm"}>Login</Link>
