0994fe2691f79d86db78d09a7061a1db459f8f22
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-03-29 20:50:55 +0900

    refactor:token localstorage save

--- src/components/boardWrite/Editor.js
+++ src/components/boardWrite/Editor.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+
 const Editor = ({ value, onChange }) => {
   const modules = {
     toolbar: {
