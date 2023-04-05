19e0d3aeba1aab2ec2b4ee80c55e5f8ae2001697
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-04-05 17:51:04 +0900

    fix:img post

--- src/components/boardWrite/Editor.js
+++ src/components/boardWrite/Editor.js
@@ -1,66 +1,112 @@
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+// axios
+import axios from "axios";
+import { useMemo, useRef, useState } from "react";
 
-const Editor = ({ value, onChange, handleImageUpload }) => {
-  const modules = {
-    toolbar: {
-      container: [
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-        [{ font: [] }],
-        [{ align: [] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
-        [{ list: "ordered" }, { list: "bullet" }, "link"],
-        [
-          {
-            color: [
-              "#000000",
-              "#e60000",
-              "#ff9900",
-              "#ffff00",
-              "#008a00",
-              "#0066cc",
-              "#9933ff",
-              "#ffffff",
-              "#facccc",
-              "#ffebcc",
-              "#ffffcc",
-              "#cce8cc",
-              "#cce0f5",
-              "#ebd6ff",
-              "#bbbbbb",
-              "#f06666",
-              "#ffc266",
-              "#ffff66",
-              "#66b966",
-              "#66a3e0",
-              "#c285ff",
-              "#888888",
-              "#a10000",
-              "#b26b00",
-              "#b2b200",
-              "#006100",
-              "#0047b2",
-              "#6b24b2",
-              "#444444",
-              "#5c0000",
-              "#663d00",
-              "#666600",
-              "#003700",
-              "#002966",
-              "#3d1466",
-              "custom-color",
-            ],
+const Editor = () => {
+  const [contentValue, setContentValue] = useState("");
+  const quillRef = useRef();
+  const imageHandler = () => {
+    console.log("핸들러시작");
+
+    const input = document.createElement("input");
+
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+    input.addEventListener("change", async () => {
+      console.log("온체인지");
+      const file = input.files[0];
+
+      const formData = new FormData();
+      formData.append("photo", file);
+      console.log(formData);
+      try {
+        let accessToken = localStorage.getItem("accessToken");
+        let refreshToken = localStorage.getItem("refreshToken");
+        const response = await axios.post("/api/post/upload", formData, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+
+            "Content-Type": "multipart/form-data",
           },
-          { background: [] },
-        ],
-        ["image", "video"],
-        ["clean"],
-      ],
-      image: {
-        uploadCallback: handleImageUpload, // 이미지가 업로드될 때마다 onImageUpload 함수 호출
-      },
-    },
+          withCredentials: true,
+          data: { grant_type: "refresh_token", refresh_token: refreshToken },
+        });
+        console.log("성공 시, 백엔드가 보내주는 데이터", response.data.img);
+        const IMG_URL = response.data.img;
+
+        const editor = quillRef.current.getEditor();
+        const range = editor.getSelection();
+        editor.insertEmbed(range.index, "image", IMG_URL);
+      } catch (error) {
+        console.log("실패");
+      }
+    });
   };
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          [{ font: [] }],
+          [{ align: [] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ list: "ordered" }, { list: "bullet" }, "link"],
+          [
+            {
+              color: [
+                "#000000",
+                "#e60000",
+                "#ff9900",
+                "#ffff00",
+                "#008a00",
+                "#0066cc",
+                "#9933ff",
+                "#ffffff",
+                "#facccc",
+                "#ffebcc",
+                "#ffffcc",
+                "#cce8cc",
+                "#cce0f5",
+                "#ebd6ff",
+                "#bbbbbb",
+                "#f06666",
+                "#ffc266",
+                "#ffff66",
+                "#66b966",
+                "#66a3e0",
+                "#c285ff",
+                "#888888",
+                "#a10000",
+                "#b26b00",
+                "#b2b200",
+                "#006100",
+                "#0047b2",
+                "#6b24b2",
+                "#444444",
+                "#5c0000",
+                "#663d00",
+                "#666600",
+                "#003700",
+                "#002966",
+                "#3d1466",
+                "custom-color",
+              ],
+            },
+            { background: [] },
+          ],
+          ["image", "video"],
+          ["clean"],
+        ],
+        handlers: {
+          // 이미지 처리는 우리가 직접 imageHandler라는 함수로 처리할 것이다.
+          image: imageHandler,
+        },
+      },
+    };
+  }, []);
   const formats = [
     //'font',
     "header",
@@ -79,16 +125,27 @@ const Editor = ({ value, onChange, handleImageUpload }) => {
     "background",
   ];
 
+  const handleButtonClick = () => {
+    const editor = quillRef.current.getEditor();
+    console.log(editor.root);
+    console.log("안의 내용물 전부", quillRef.current.getEditorContents());
+  };
+
   return (
-    <ReactQuill
-      placeholder="내용을 입력해주세요"
-      className="w-5/6  mx-auto h-96"
-      theme="snow"
-      modules={modules}
-      formats={formats}
-      value={value || ""}
-      onChange={(content, delta, sosurce, editor) => onChange(editor.getHTML())}
-    />
+    <div>
+      {" "}
+      <ReactQuill
+        placeholder="내용을 입력해주세요"
+        className="w-5/6  mx-auto h-96"
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        value={contentValue || ""}
+        ref={quillRef}
+        onChange={setContentValue}
+      />
+      <button onClick={handleButtonClick}>에디터 안의 내용들</button>
+    </div>
   );
 };
 
